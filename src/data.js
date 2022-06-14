35e6c87a211bd7ff8a86e8cb3a901186dfe7622c
dedyhsaputra <deddyisutin@gmail.com>
2022-06-14 22:30:08 +0700

    first commit

---
+++ src/data.js
@@ -0,0 +1,76 @@
+export const sliderItem = [
+  {
+    id: 1,
+    img: "https://img.freepik.com/free-photo/covid19-shop-concept-young-asian-stylish-woman-wearing-medical-face-mask-holding-shopping-bag-going-malls-during-pandemic-white-background_1258-90290.jpg?t=st=1655048149~exp=1655048749~hmac=5d0040196ef0cf027d5763b33455ef46db4d69bd5133ccd639f9a516d0133e96&w=1060",
+    title: "SUMMER SALE",
+    desc: "DON'T COMPROMISE ON STYLE GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: "https://img.freepik.com/free-photo/young-beautiful-muslim-woman-suit-holding-colorful-shopping-bags-white-background-studio_33799-15083.jpg?w=1060",
+    title: "AUTUMN COLLECTION",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img: "https://img.freepik.com/free-photo/stylish-young-woman-posing-with-shopping-bags-after-great-shopping_176420-9041.jpg?t=st=1655048149~exp=1655048749~hmac=8cc8c1c663e171b4b78e683ab109f18169bf194491190b56cee7c5b3d571f6da&w=1060",
+    title: "WINTER SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "EEEEEE",
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    img: "https://images.pexels.com/photos/2220319/pexels-photo-2220319.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    title: "SHIRT STYLE",
+  },
+  {
+    id: 2,
+    img: "https://images.pexels.com/photos/2220314/pexels-photo-2220314.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    title: "LOUNGEWEAR LOVE",
+  },
+  {
+    id: 3,
+    img: "https://images.pexels.com/photos/2220315/pexels-photo-2220315.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    title: "LIGHT JACKETS",
+  },
+];
+
+export const popularProducts = [
+  {
+    id: 1,
+    img: "https://www.pngarts.com/files/12/Paper-Bag-PNG-Image-Background-1.png",
+  },
+  {
+    id: 2,
+    img: "https://www.pngarts.com/files/1/T-Shirt-Transparent-Background-PNG.png",
+  },
+  {
+    id: 3,
+    img: "https://www.pngarts.com/files/1/T-Shirt-With-A-Heart-PNG-Download-Image.png",
+  },
+  {
+    id: 4,
+    img: "https://www.pngarts.com/files/5/Black-Jacket-Download-PNG-Image.png",
+  },
+  {
+    id: 5,
+    img: "https://www.pngarts.com/files/5/Black-Jacket-Free-PNG-Image.png",
+  },
+  {
+    id: 6,
+    img: "https://www.pngarts.com/files/12/Bandit-Balaclava-PNG-Download-Image.png",
+  },
+  {
+    id: 7,
+    img: "https://www.pngarts.com/files/2/Obey-Cap-PNG-Picture.png",
+  },
+  {
+    id: 8,
+    img: "https://www.pngarts.com/files/12/Sports-Bag-PNG-Image.png",
+  },
+];
